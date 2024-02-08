a445488d01a11395f1caa121bfe91e660e6a0ff6
Sherif <sherifmuhammed0@gmail.com>
2024-02-07 17:45:58 -0800

    Change GetTotalHoursPerDayUseCase to return Float and add GetTotalHoursPerDayAsStringUseCase.

--- app/src/main/java/me/sherief/attendease/domain/usecase/GetTotalHoursPerDayUseCase.kt
+++ app/src/main/java/me/sherief/attendease/domain/usecase/GetTotalHoursPerDayUseCase.kt
@@ -3,15 +3,26 @@ package me.sherief.attendease.domain.usecase
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-class GetTotalHoursPerDayUseCase {
+class GetTotalHoursPerDayAsStringUseCase {
     operator fun invoke(
         timeIn: Long,
         timeOut: Long
     ): String {
         val diffMillis = timeOut - timeIn
         val duration = diffMillis.toDuration(DurationUnit.MILLISECONDS)
-        return duration.toComponents { hours, minutes, _ ->
-            "%02d:%02d".format(hours, minutes)
+        return duration.toComponents { hours, minutes, seconds, _ ->
+            "%02d:%02d".format(hours, minutes, seconds)
         }
+    }
+}
+
+class GetTotalHoursPerDayUseCase {
+    operator fun invoke(
+        timeIn: Long,
+        timeOut: Long
+    ): Float {
+        val diffMillis = timeOut - timeIn
+        val duration = diffMillis.toDuration(DurationUnit.MILLISECONDS)
+        return duration.inWholeHours.toFloat()
     }
 }
