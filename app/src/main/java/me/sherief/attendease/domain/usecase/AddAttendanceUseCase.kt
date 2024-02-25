53a0a07abe1951eaf6c3fef87bd9298576358474
Sherif <sherifmuhammed0@gmail.com>
2024-02-24 22:53:28 -0800

    Change the implementation of AddAttendanceUseCase after delete GetTotalHoursPerDayUseCase.

--- app/src/main/java/me/sherief/attendease/domain/usecase/AddAttendanceUseCase.kt
+++ app/src/main/java/me/sherief/attendease/domain/usecase/AddAttendanceUseCase.kt
@@ -3,17 +3,17 @@ package me.sherief.attendease.domain.usecase
 import me.sherief.attendease.domain.model.Attendance
 import me.sherief.attendease.domain.repository.AttendanceRepository
 import me.sherief.attendease.domain.util.Result
+import me.sherief.attendease.domain.util.doTry
+import me.sherief.attendease.domain.util.flatMap
 import javax.inject.Inject
 
 
-class AddAttendanceUseCase @Inject constructor(
-    private val attendanceRepository: AttendanceRepository,
-    private val getTotalHoursPerDayUseCase: GetTotalHoursPerDayUseCase
-) {
-    suspend operator fun invoke(attendance: Attendance): Result<Attendance> {
-        val totalHours = getTotalHoursPerDayUseCase(attendance.timeIn, attendance.timeOut)
-        val updatedAttendance = attendance.copy(totalHours = totalHours)
-        return attendanceRepository.addAttendance(updatedAttendance)
+class AddAttendanceUseCase @Inject constructor(private val attendanceRepository: AttendanceRepository) {
+    suspend operator fun invoke(attendance: Attendance): Result<Attendance> =
 
-    }
+        doTry {
+            attendance
+        }.flatMap { validAttendance ->
+            attendanceRepository.addAttendance(validAttendance)
+        }
 }
