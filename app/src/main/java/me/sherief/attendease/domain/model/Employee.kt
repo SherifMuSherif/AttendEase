ee3cf021ca66bb07f4fb6da1ca1d6992fb9f6875
Sherif <sherifmuhammed0@gmail.com>
2024-02-26 20:00:45 -0800

    Make _timeOut as private var, add updateTimeOut method to Attendance, and add documentation for both Attendance and HoursAndMinutes classes.

--- app/src/main/java/me/sherief/attendease/domain/model/Employee.kt
+++ app/src/main/java/me/sherief/attendease/domain/model/Employee.kt
@@ -14,32 +14,80 @@ data class Employee(
     val attendanceHistory: List<Attendance>?
 )
 
+/**
+ * A class that represents the attendance record of an employee on a given date.
+ *
+ * @property attendanceId A unique identifier for the attendance record.
+ * @property employeeId The employee's ID.
+ * @property date The date of the attendance.
+ * @property timeIn The time when the employee checked in.
+ * @property _timeOut The time when the employee checked out.
+ * @property _totalHoursAndMinutes The total hours and minutes worked by the employee on that date, calculated from [timeIn] and [_timeOut].
+ * @constructor Initializes the properties of the [Attendance] object and performs validations and calculations.
+ * @throws InvalidTimeRangeException If [timeOut] is not after [timeIn].
+ */
 data class Attendance(
     val attendanceId: String,
     val employeeId: String,
     val date: LocalDate,
     val timeIn: LocalDateTime,
-    val timeOut: LocalDateTime,
+    private var _timeOut: LocalDateTime? = null,
     private var _totalHoursAndMinutes: HoursAndMinutes = HoursAndMinutes(0, 0)
 ) {
     init {
-        if (!isTimeOutAfterTimeIn()) throw InvalidTimeRangeException("timeOut must be after timeIn.")
-        if (_totalHoursAndMinutes == HoursAndMinutes(0, 0))
-            _totalHoursAndMinutes = calculateTotalHours()
+        if (_timeOut != null) {
+            if (isTimeOutAfterTimeIn().not()) throw InvalidTimeRangeException("timeOut must be after timeIn.")
+            if (_totalHoursAndMinutes == HoursAndMinutes(0, 0))
+                _totalHoursAndMinutes = calculateTotalHours()
+        }
     }
 
     val totalHoursAndMinutes: HoursAndMinutes get() = _totalHoursAndMinutes
+    val timeOut: LocalDateTime? get() = _timeOut
 
-    private fun calculateTotalHours() = timeOut durationFromEndToStart timeIn
-    private fun isTimeOutAfterTimeIn() = (timeOut > timeIn)
+    /**
+     * Returns the total hours and minutes worked by the employee on that date as a [HoursAndMinutes] object.
+     */
+    private fun calculateTotalHours() = _timeOut!! durationFromEndToStart timeIn
+
+    /**
+     * Checks if a given [targetTimeOut] is after [timeIn] and returns a boolean value.
+     *
+     * @param targetTimeOut A [LocalDateTime] value that represents the time to compare with [timeIn]. If not specified, it defaults to [_timeOut].
+     * @return A boolean value indicating whether [targetTimeOut] is after [timeIn] or not.
+     */
+    private fun isTimeOutAfterTimeIn(targetTimeOut: LocalDateTime? = _timeOut) =
+        (targetTimeOut!! > timeIn)
+
+    /**
+     * Updates the [_timeOut] property with a new value and recalculates the [totalHoursAndMinutes] property accordingly.
+     *
+     * @param newTimeOut A [LocalDateTime] value that represents the new time when the employee checked out.
+     * @throws InvalidTimeRangeException If [newTimeOut] is not after [timeIn].
+     */
+    fun updateTimeOut(newTimeOut: LocalDateTime) {
+        if (isTimeOutAfterTimeIn(newTimeOut).not()) throw InvalidTimeRangeException("timeOut must be after timeIn.")
+        _timeOut = newTimeOut
+        _totalHoursAndMinutes = calculateTotalHours()
+    }
 }
 
+/**
+ * A class that represents a duration of time in hours and minutes.
+ *
+ * @property hours The number of hours in the duration.
+ * @property minutes The number of minutes in the duration.
+ * @constructor Initializes the properties of the [HoursAndMinutes] object and adjusts them if [minutes] is 60 or more.
+ */
 data class HoursAndMinutes(private var hours: Long, private var minutes: Int) {
     init {
         // If total minutes is 60 or more, convert it to hours
         adjustTimeForMinutesOverflow()
     }
 
+    /**
+     * Adds another [HoursAndMinutes] object to the current one and assigns the result to the current one.
+     */
     operator fun plusAssign(other: HoursAndMinutes) {
         this.hours += other.hours
         this.minutes += other.minutes
@@ -48,6 +96,9 @@ data class HoursAndMinutes(private var hours: Long, private var minutes: Int) {
         adjustTimeForMinutesOverflow()
     }
 
+    /**
+     * Adjusts the [hours] and [minutes] properties if [minutes] is 60 or more by subtracting 60 from [minutes] and adding 1 to [hours].
+     */
     private fun adjustTimeForMinutesOverflow() {
         if (this.minutes >= 60) {
             this.minutes -= 60
